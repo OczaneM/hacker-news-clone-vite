4f87586e4e79c14c811b972475951b00dcd03d07
OczaneM <oczane.rivera@gmail.com>
2022-11-25 14:36:56 -0500

    Add layout styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = ({ storyIds }) => {
         {storyIds.map((storyId, index) => {
           return <Story key={storyId} storyId={storyId} index={index + 1} />
         })}
-        <button>show more</button>
+        <button className="showmore">show more</button>
       </div>
       <div className="footer">
         <div className="title">Hacker News</div>
