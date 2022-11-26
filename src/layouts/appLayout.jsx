23697f99ed45b5fe352512458a60d0b2e54370f5
OczaneM <oczane.rivera@gmail.com>
2022-11-25 23:52:36 -0500

    enable darkmode

--- src/layouts/appLayout.jsx
+++ src/layouts/appLayout.jsx
@@ -1,12 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { Outlet } from "react-router-dom"
 import "./appLayout.scss"
 import "../styles/globalOverrides.scss"
+import moonIcon from "../assets/icons8-moon-symbol-30.png"
+import sunIcon from "../assets/icons8-sun.svg"
 
 const AppLayout = () => {
+  const [darkMode, setDarkMode] = useState(false)
+
   return (
-    <div className="theme -classic">
+    <div className={`theme -classic ${darkMode && "-darkmode"}`}>
+      <button className="themetoggle" onClick={() => setDarkMode(!darkMode)}>
+        <img src={darkMode ? sunIcon : moonIcon} />
+      </button>
       <Outlet />
     </div>
   )
