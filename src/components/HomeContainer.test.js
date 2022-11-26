27e281b117fe823cdd27bd902432f6dee76d233e
OczaneM <oczane.rivera@gmail.com>
2022-11-26 01:01:59 -0500

    Install and setup babel

--- src/components/HomeContainer.test.js
+++ src/components/HomeContainer.test.js
@@ -1,29 +1,39 @@
-import { render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
+import React from "react"
+import { screen } from "@testing-library/react"
 import { rest } from "msw"
 import { setupServer } from "msw/node"
+import { renderWithProviders } from "../utils/testUtils"
 import HomeContainer from "./HomeContainer"
 
-// We use msw to intercept the network request during the test,
-// and return the response 'John Smith' after 150ms
-// when receiving a get request to the `/api/user` endpoint
-export const handlers = [
-  rest.get("/api/user", (req, res, ctx) => {
-    return res(ctx.json("John Smith"), ctx.delay(150))
-  }),
-]
+const storyIds = [1, 2, 3]
+// // We use msw to intercept the network request during the test,
+// // and return the response 'John Smith' after 150ms
+// // when receiving a get request to the `/api/user` endpoint
+// export const handlers = [
+//   rest.get(
+//     "https://hacker-news.firebaseio.com/v0/newstories.json",
+//     (req, res, ctx) => {
+//       return res(storyIds, ctx.delay(150))
+//     }
+//   ),
+// ]
 
-const server = setupServer(...handlers)
+// const server = setupServer(...handlers)
 
-// Enable API mocking before tests.
-beforeAll(() => server.listen())
+// // Enable API mocking before tests.
+// beforeAll(() => server.listen())
 
-// Reset any runtime request handlers we may add during the tests.
-afterEach(() => server.resetHandlers())
+// // Reset any runtime request handlers we may add during the tests.
+// afterEach(() => server.resetHandlers())
 
-// Disable API mocking after the tests are done.
-afterAll(() => server.close())
+// // Disable API mocking after the tests are done.
+// afterAll(() => server.close())
 
 describe("HomeContainer", () => {
-  test("Displays a home page", () => {})
+  test("Displays a home page after succesfully fetching stories", () => {
+    renderWithProviders(<HomeContainer />)
+
+    expect(screen.getByText("Hacker News")).toBeVisible()
+  })
 })
